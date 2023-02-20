57d81fcb77832d165088e1f29fce84674843ad54
Alessandro Mazzon <alessandro@forbole.com>
2023-02-20 15:43:10 +0100

    chore: added env file

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,16 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MEASUREMENT_ID,
+  MESSAGGIN_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from "@env";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyB05ZGdbTJ4bcukvVO3J0_c4dLFrtmbIJc",
-  authDomain: "firetu-9fd88.firebaseapp.com",
-  projectId: "firetu-9fd88",
-  storageBucket: "firetu-9fd88.appspot.com",
-  messagingSenderId: "797872658597",
-  appId: "1:797872658597:web:f8ca57fe4b81e500c1154c",
-  measurementId: "G-QWB5PSN89G",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGGIN_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase and Firestore
